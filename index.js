8a3ec36223786e70c0bbf6c9111e4d866beca23d
mdzaforiqbal <78316937+mdzaforiqbal@users.noreply.github.com>
2021-01-31 19:18:22 +0100

    javascript für Taschenrechner

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿                                                                                                                          
+
+
+function insertNumber(number) {
+
+    var existingNumbers = $("#result").val();
+    $("#result").val(existingNumbers + number)
+
+}
+
+
+
+
+function clearResult(){
+
+    $("#result").val('')
+
+
+}
+
+
+
+ 
+
+
+   function calculate(){
+
+      var result = eval($("#result").val() )
+
+       $("#result").val(result)
+
+}
+
+
+
